b10898ec2bcf1e4ed56e9f087b0925a8632388b2
DJ <heyymrdj@protonmail.com>
2024-01-28 17:30:12 -0500

    Added Oauth, added forms, updated formatting

--- cmd/integral/main.go
+++ cmd/integral/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+    "io"
+    "golang.org/x/oauth2"
+    "context"
     "path/filepath"
 	"database/sql"
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -21,6 +23,23 @@ var dbUser = os.Getenv("DB_USER")
 var dbPassword = os.Getenv("DB_PASSWORD")
 var dbName = os.Getenv("DB_NAME")
 
+var clientid = os.Getenv("CLIENT_ID")
+var clientSecret = os.Getenv("CLIENT_SECRET")
+var realm = os.Getenv("REALM")
+
+var oauthConfig = &oauth2.Config{
+    RedirectURL:  "http://localhost:8080/callback",
+    ClientID:     clientid,
+    ClientSecret: clientSecret,
+    Scopes:       []string{"openid", "profile", "email"},
+    Endpoint: oauth2.Endpoint{
+        AuthURL:  "http://localhost:8069/realms/myrealm/protocol/openid-connect/auth",
+        TokenURL: "http://localhost:8069/realms/myrealm/protocol/openid-connect/token",
+    },
+}
+
+var oauthStateString = "random"
+
 func main() {
     // Serve static files
     fs := http.FileServer(http.Dir("web/static"))
@@ -31,6 +50,11 @@ func main() {
     http.HandleFunc("/", testHandler)
     http.HandleFunc("/upload", uploadFileHandler)
     http.HandleFunc("/delete", deletePhotosHandler)
+    http.HandleFunc("/servedelete", serveDelete)
+    http.HandleFunc("/update", serveUpdate)
+    http.HandleFunc("/create", serveCreate)
+    http.HandleFunc("/login", loginHandler)
+    http.HandleFunc("/callback", callbackHandler)
 
     // Start the server
     log.Println("Server starting on :8080")
@@ -132,7 +156,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
     // Parse the template
     images2 := listPhotos()
     fmt.Println(images2)
-    tmpl, err := template.ParseFiles("./web/template/base.html", "./web/template/test.html")
+    tmpl, err := template.ParseFiles("./web/template/base.html", "./web/template/list.html")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -170,4 +194,106 @@ func deleteFiles(directory string) error {
     }
 
     return nil
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    url := oauthConfig.AuthCodeURL(oauthStateString, oauth2.AccessTypeOffline)
+    http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+    // Check that the state parameter matches
+    state := r.FormValue("state")
+    if state != oauthStateString {
+        fmt.Printf("invalid oauth state, expected %s, got %s\n", oauthStateString, state)
+        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+        return
+    }
+
+    code := r.FormValue("code")
+    token, err := oauthConfig.Exchange(context.Background(), code)
+    if err != nil {
+        fmt.Printf("oauthConfig.Exchange() failed with %s\n", err)
+        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+        return
+    }
+    // Create a new HTTP client
+        client := oauthConfig.Client(context.Background(), token)
+
+        // Define the user info endpoint of the OAuth provider
+        // This will vary depending on the provider and should be replaced with the actual endpoint
+        userInfoURL := "http://localhost:8069/realms/myrealm/protocol/openid-connect/userinfo"
+
+        // Make a request to the user info endpoint
+        resp, err := client.Get(userInfoURL)
+        if err != nil {
+            // Handle error
+            fmt.Printf("Error retrieving user info: %s\n", err)
+            http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+            return
+        }
+        defer resp.Body.Close()
+
+        // Read and process the response
+        // The exact processing will depend on the structure of the response
+        userInfo, err := io.ReadAll(resp.Body)
+        if err != nil {
+            // Handle error
+            fmt.Printf("Error reading user info response: %s\n", err)
+            http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+            return
+        }
+
+        // Do something with the user info, e.g., display it or create a user session
+        fmt.Fprintf(w, "User Info: %s", string(userInfo))
+}
+
+func serveCreate(w http.ResponseWriter, r *http.Request) {
+    // Parse the template
+    images2 := listPhotos()
+    fmt.Println(images2)
+    tmpl, err := template.ParseFiles("./web/template/base.html", "./web/template/create.html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Execute the template
+    err = tmpl.Execute(w, images2)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+func serveDelete(w http.ResponseWriter, r *http.Request) {
+    // Parse the template
+    images2 := listPhotos()
+    fmt.Println(images2)
+    tmpl, err := template.ParseFiles("./web/template/base.html", "./web/template/delete.html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Execute the template
+    err = tmpl.Execute(w, images2)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func serveUpdate(w http.ResponseWriter, r *http.Request) {
+    // Parse the template
+    images2 := listPhotos()
+    fmt.Println(images2)
+    tmpl, err := template.ParseFiles("./web/template/base.html", "./web/template/edit.html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Execute the template
+    err = tmpl.Execute(w, images2)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/OpenProductsllc/integral
 
 go 1.21.3
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/oauth2 v0.16.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
